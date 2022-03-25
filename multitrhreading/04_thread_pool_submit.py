abd156d6944618ecff0eede5a40a4dc06cb17cad
Egor Sidorov <flatnoy@gmail.com>
2022-03-25 17:54:26 +0300

    129

--- multitrhreading/04_thread_pool_submit.py
+++ multitrhreading/04_thread_pool_submit.py
@@ -3,22 +3,41 @@ import time
 
 
 def div(divisor, limit):
-    print(f'started div={divisor}')
+    print(f'\nstarted div={divisor}')
 
+    result = 0
     for x in range(1, limit):
         if x % divisor == 0:
-            print(f'divisor={divisor}, x={x}')
+            result += x
+            # print(f'divisor={divisor}, x={x}')
         time.sleep(0.2)
-    print(f'ended div={divisor}', end='\n')
+    print(f'\nended div={divisor}', end='\n')
+    return result
 
 
 if __name__ == '__main__':
     print('started main')
 
+    futures = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(div, 3, 25)
-        executor.submit(div, 5, 25)
+        futures.append(executor.submit(div, 5, 25))
+        futures.append(executor.submit(div, 3, 25))
+
+        while futures[0].running() and futures[1].running():
+            print('.', end='')
+            time.sleep(0.5)
+
+        for f in futures:
+            print(f'{f.result()}')
 
         print('Immediately printed out after submit')
 
     print('After with block')
+
+    # executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    # executor.submit(div, 3, 25)
+    # executor.submit(div, 5, 25)
+    #
+    # executor.shutdown(wait=False)
+
+    print('\nmain ended')
