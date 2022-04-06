0ec5e7657336e04f88ffbc0f60de7a199d3fb291
Egor Sidorov <flatnoy@gmail.com>
2022-04-07 00:12:37 +0300

    129 1401 14 041

---
+++ asyncio/01_00_first_example.py
@@ -0,0 +1,21 @@
+import asyncio
+# import time
+
+from multitrhreading.decorators import async_measure_time
+
+
+async def tick():
+    print('Tick')
+    await asyncio.sleep(1)
+    print('Tock')
+
+
+@async_measure_time
+async def main():
+    await asyncio.gather(tick(), tick(), tick())
+    # for _ in range(3):
+    #     tick()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
