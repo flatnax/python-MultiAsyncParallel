c5482537db5891d0360b37854d0c3ece62d2c898
Egor Sidorov <flatnoy@gmail.com>
2022-08-09 17:39:02 +0300

    129 1401 14 041

---
+++ asyncio/05_02_task_as_complited.py
@@ -0,0 +1,29 @@
+import asyncio
+import time
+
+
+async def tick():
+    await asyncio.sleep(1)
+    return "Tick"
+
+
+async def tock():
+    await asyncio.sleep(2)
+    return "Tock"
+
+
+async def main():
+    start = time.perf_counter()
+
+    t1 = asyncio.create_task(tick())
+    t2 = asyncio.create_task(tock())
+
+    results = await asyncio.gather(t1, t2)
+
+    for i, result in enumerate(results, start=1):
+        elapsed = time.perf_counter() - start
+        print(f'Executed {i} in {elapsed:0.2f} seconds')
+        print(result)
+
+if __name__ == '__main__':
+    asyncio.run(main())
